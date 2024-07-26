4fd3e703c7bb0639882f377b61fdb5bf80b337e3
TechEdDan2 <dg.nelson2@gmail.com>
2024-07-26 08:45:25 -0400

    modified app.js with the getGifByTerm function and added some elements to the index file. Created the css file

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("Let's get this party started!");
+// Key: r4KF5WGYjnw6abjVL3JN3JS8TeJETHjQ 
+const key = 'r4KF5WGYjnw6abjVL3JN3JS8TeJETHjQ';
+const searchForm = document.querySelector('#searchform');
+const userInput = document.querySelector('#search');
+
+/**
+ * 
+ * @param {*} term 
+ */
+async function getGifByTerm(term) {
+    try {
+        const searchUrl = `http://api.giphy.com/v1/gifs/random?q=${term}&api_key=${key}`;
+        const result = await axios.get(searchUrl);
+        console.log(result.data.data.url);
+
+    } catch (e) {
+        alert(`Your search for ${term} didn't work`);
+    }
+}
+
+
