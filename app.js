54461ac20b5c9b3b89ff626d5d006a2325385c22
TechEdDan2 <dg.nelson2@gmail.com>
2024-07-26 09:40:01 -0400

    Added some new functions, but they have some bugs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log("Let's get this party started!");
 const key = 'r4KF5WGYjnw6abjVL3JN3JS8TeJETHjQ';
 const searchForm = document.querySelector('#searchform');
 const userInput = document.querySelector('#search');
+const gifBox = document.querySelector('#imgBox');
 
 /**
  * 
@@ -13,10 +14,24 @@ async function getGifByTerm(term) {
         const searchUrl = `http://api.giphy.com/v1/gifs/random?q=${term}&api_key=${key}`;
         const result = await axios.get(searchUrl);
         console.log(result.data.data.url);
-
+        return result.data.data.url;
     } catch (e) {
         alert(`Your search for ${term} didn't work`);
     }
 }
 
+function newGifImg(url) {
+    console.log(url);
+    $('<img>').attr('src', url).appendTo('#imgBox');
+}
 
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const userTerm = userInput.value;
+    console.log(userTerm);
+
+    console.log(getGifByTerm(userTerm));
+
+    newGifImg(getGifByTerm(userTerm).toString());
+
+});
