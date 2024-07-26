f8556d94d624ea30e9dea643bcaa26ad0aefbcbe
TechEdDan2 <dg.nelson2@gmail.com>
2024-07-26 19:49:50 -0400

    Final Changes

--- app2.js
+++ app2.js
@@ -1,46 +1,47 @@
-console.log("Let's get this party started!");
 // Key: r4KF5WGYjnw6abjVL3JN3JS8TeJETHjQ 
 const key = 'r4KF5WGYjnw6abjVL3JN3JS8TeJETHjQ';
-const searchForm = document.querySelector('#searchform');
 const userInput = document.querySelector('#search');
+const searchForm = document.querySelector('#searchform');
 const $gifBox = $('#imgBox');
 
 /**
+ * The newGifImg function accepts a URL and uses
+ *  that data when creating a new HTML image 
+ *  element. That new image is then appended a 
+ *  div where all images will be displayed.
  * 
- * @param {*} imgUrl 
+ * @param {string} imgUrl   Contains a URL 
  */
 function newGifImg(imgUrl) {
     let $newImg = $('<img>', {
-        src: imgUrl
+        src: imgUrl,
+        class: 'imgStyle'
     });
     $newImg.appendTo('#imgBox');
 }
 
 /**
- * 
+ * This block of code uses vanilla javascript to
+ * create an event handler to extract the user's
+ * keyword from the text input field and incorporate
+ * the data along with the key to submit a get request
+ * to Giphy for gifs
  */
 searchForm.addEventListener('submit', async function (e) {
     e.preventDefault();
 
+    // access the text in the text input
     let userTerm = userInput.value;
-    console.log(userTerm);
-    userInput.value = '';
+    userInput.value = '';//reset text
 
     // search for a random image that matches the target term
     try {
         const searchUrl = `http://api.giphy.com/v1/gifs/search?q=${userTerm}&api_key=${key}`;
         const results = await axios.get(searchUrl);
 
-        console.log(results.data);
-
         let numResults = results.data.data.length;
-        console.log(numResults);
-
         let randomNum = Math.floor(Math.random() * numResults);
-
         randomUrl = results.data.data[randomNum].images.downsized.url;
-
-        console.log(randomUrl);
 
         newGifImg(randomUrl);
     } catch (e) {
@@ -49,7 +50,9 @@ searchForm.addEventListener('submit', async function (e) {
 });
 
 /**
- * 
+ * This block of code uses jQuery syntax to handle 
+ *  the "Delete" event, which removes all images 
+ *  on the page.
  */
 $('#delete').on('click', function () {
     $gifBox.empty();
